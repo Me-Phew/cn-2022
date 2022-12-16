d5dc3bccf48ddc4a32939988fcfb9fa62e312bba
Mateusz <basmateusz@wp.pl>
2022-12-17 00:07:12 +0100

    Configure CORS to allow credentials

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,41 +8,50 @@ import { registerRoutes } from './utils/routes/registerRoutes';
 import { errorHandler } from './errors';
 import passport from './auth/school.auth';
 
-AppDataSource.initialize().then(async () => {
-    const { PORT } = process.env;
-    const app = express();
+AppDataSource.initialize()
+    .then(async () => {
+        const { PORT } = process.env;
+        const app = express();
 
-    app.use(cors());
-    app.use(json());
-    app.use(session({
-        secret: 'test',
-        saveUninitialized: false,
-        resave: false,
-    }));
-    app.use(passport.authenticate('session'));
-    swaggerDocs(app, PORT as unknown as number);
-    registerRoutes(app, routes);
+        app.use(
+            cors({
+                origin: 'http://localhost:5173',
+                credentials: true,
+            })
+        );
+        app.use(json());
+        app.use(
+            session({
+                secret: 'test',
+                saveUninitialized: false,
+                resave: false,
+            })
+        );
+        app.use(passport.authenticate('session'));
+        swaggerDocs(app, PORT as unknown as number);
+        registerRoutes(app, routes);
 
-    /**
-     * @openapi
-     * /:
-     *  get:
-     *     tags:
-     *     - Root
-     *     description: Root route
-     *     responses:
-     *       200:
-     *         description: App is up and running
-     */
-    app.get('/', (_, res) => {
-        res.send('Welcome buddy! Let\'s chop some wood together!');
-    });
+        /**
+         * @openapi
+         * /:
+         *  get:
+         *     tags:
+         *     - Root
+         *     description: Root route
+         *     responses:
+         *       200:
+         *         description: App is up and running
+         */
+        app.get('/', (_, res) => {
+            res.send("Welcome buddy! Let's chop some wood together!");
+        });
 
-    app.use(errorHandler);
+        app.use(errorHandler);
 
-    app.listen(PORT, () => {
-        console.log('Hello!');
-    }).on('error', (error) => {
-        console.error(error);
-    });
-}).catch((error) => console.log(error));
+        app.listen(PORT, () => {
+            console.log('Hello!');
+        }).on('error', (error) => {
+            console.error(error);
+        });
+    })
+    .catch((error) => console.log(error));
