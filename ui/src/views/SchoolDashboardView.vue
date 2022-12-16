fdde28fd06a9fc0b1c3301db59fdd981c49dcae4
biQte <patrykgonet@gmail.com>
2022-12-16 20:07:00 +0100

    Add navbar in school dashboard

---
+++ ui/src/views/SchoolDashboardView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheNavbar from '@/components/TheNavbar.vue';
+</script>
+
+<template>
+    <div>
+        <TheNavbar />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
