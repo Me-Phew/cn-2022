c337a5104347de45d926be19440fdd7991b2deb9
Bartek <pzpsofficial@gmail.com>
2022-12-17 08:31:30 +0100

    creating user

--- api/src/controllers/student.controller.ts
+++ api/src/controllers/student.controller.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from '../data-source';
 import { Student } from '../entity/Student';
 import { Password } from '../entity/Password';
+import { RegistrationCode } from '../entity/RegistrationCode';
 import { NextFunction, Request, Response } from 'express';
 import { hashPassword } from 'utils/password';
 import { validate } from 'class-validator';
@@ -9,7 +10,12 @@ import { ValidationError } from '../errors';
 export class StudentController {
     static async create(req: Request, res: Response, next: NextFunction) {
         try {
+            const passwordRepo = AppDataSource.getRepository(Password);
             const studentsRepo = AppDataSource.getRepository(Student);
+            const codesRepo = AppDataSource.getRepository(RegistrationCode);
+            const code = await codesRepo.findOneBy({
+                value: req.body.code
+            });
             const student = new Student();
             const password = new Password();
             password.value = await hashPassword(req.body.password, req.body.passwordConfirm);
@@ -25,6 +31,16 @@ export class StudentController {
             if (errors.length > 0) {
                 throw new ValidationError(errors);
             }
+
+            if (code) {
+                code.wasUsed = true;
+                await codesRepo.save(code);
+            }
+
+            await passwordRepo.save(password);
+            student.password = [password];
+            await studentsRepo.save(student);
+            res.send({});
         } catch (error) {
             return next(error);
         }
