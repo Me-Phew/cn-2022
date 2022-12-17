f6ad988fb19919157ee71a790781db8e879f947b
Bartek <pzpsofficial@gmail.com>
2022-12-17 08:07:57 +0100

    auth error fix

--- api/src/errors/errorHandler.ts
+++ api/src/errors/errorHandler.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
-import { ValidationError } from './custom';
+import { ValidationError, AuthenticationError } from './custom';
 
 export function errorHandler(err: Error | ValidationError, req: Request, res: Response, next: NextFunction) {
-    if (err instanceof ValidationError) {
+    if (err instanceof ValidationError || err instanceof AuthenticationError) {
         return res.status(err.code).send(err.getError());
     }
 
