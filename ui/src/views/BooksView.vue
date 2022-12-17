358c145aee4bc4f08162925d8091174cb4e5cca8
biQte <patrykgonet@gmail.com>
2022-12-17 03:00:13 +0100

    Style book cards

--- ui/src/views/BooksView.vue
+++ ui/src/views/BooksView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-import { NButton, NCard } from 'naive-ui';
+import { NButton, NCard, NTag } from 'naive-ui';
 
 export interface CardInterface extends Object {
     title: string,
@@ -174,9 +174,15 @@ const cardsDataComputed = computed(() => {
 
     cardsData.value.forEach(cardData => {
         let availability;
-        if (cardData.quantity / cardData.available > (.25 * cardData.available)) availability = 'low ';
-        if (cardData.available === 0) availability = 'none';
-        if (!availability) availability = 'good';
+        if (cardData.quantity / cardData.available > (.25 * cardData.available)) {
+            availability = 'low ';
+        }
+        if (cardData.available === 0) {
+            availability = 'none';
+        }
+        if (!availability) {
+            availability = 'good';
+        }
 
         newCardsData.push({ ...cardData, availability: availability, });
     });
@@ -193,18 +199,26 @@ const cardsDataComputed = computed(() => {
         </router-link>
         <div class="books-wrapper">
             <div class="card" v-for="card in cardsDataComputed" :key="card.title">
-                <n-card :title="card.title">
+                <n-card :title="card.title" class="n-card">
                     <template #cover>
                         <img :src="card.imgSrc">
                     </template>
-                    <h3><strong>Autor: </strong>{{ card.author }}</h3>
-                    <h3><strong>Kategoria: </strong>{{ card.category }}</h3>
-                    <p>
-                        <strong>Dostępne: </strong>
-                        <span :class="card.availability">
-                            {{ card.available }}/{{ card.quantity }}
-                        </span>
-                    </p>
+                    <div class="tags">
+                        <n-tag type="info" size="small">
+                            <h3>{{ card.category }}</h3>
+                        </n-tag>
+                        <n-tag>
+                            <h3>Autor: {{ card.author }}</h3>
+                        </n-tag>
+                        <n-tag :bordered="false">
+                            <p>
+                                Dostępne:
+                                <span :class="card.availability">
+                                    {{ card.available }}/{{ card.quantity }}
+                                </span>
+                            </p>
+                        </n-tag>
+                    </div>
                 </n-card>
             </div>
         </div>
@@ -227,6 +241,12 @@ const cardsDataComputed = computed(() => {
 
         .card {
             max-width: 280px;
+
+            .tags {
+                display: grid;
+                flex-direction: column;
+                gap: .2rem;
+            }
 
             .good {
                 color: var(--color-accent);
