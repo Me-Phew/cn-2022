24a36f978eeb6aae5379d3330fad9d3446fc5286
biQte <patrykgonet@gmail.com>
2022-12-17 05:29:36 +0100

    Add prop for school name in navbar

--- ui/src/views/SchoolDashboardView.vue
+++ ui/src/views/SchoolDashboardView.vue
@@ -35,7 +35,7 @@ const menuItems: Array<MenuItem> = [
 
 <template>
     <div class="school-dashboard-wrapper">
-        <TheNavbar role="Administrator" name="Walenty" lastName="Szarek"
+        <TheNavbar school="ZSEM" role="Administrator" name="Walenty" lastName="Szarek"
             src="https://previews.123rf.com/images/findriyani/findriyani1807/findriyani180700241/105630149-education-people-school-logo-design-template.jpg" />
         <TheHorizontalMenu :menuItems="menuItems" />
         <RouterView name="schoolDashboard" />
