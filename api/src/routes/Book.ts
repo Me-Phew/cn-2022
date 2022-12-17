4699779e6729b1b4098b60510649fcaa37cfb128
Bartek <pzpsofficial@gmail.com>
2022-12-17 04:23:03 +0100

    thumbnail support for books

--- api/src/routes/Book.ts
+++ api/src/routes/Book.ts
@@ -1,11 +1,22 @@
 import { Router } from 'express';
 import { auth } from '../middleware/auth';
-import passport from '../auth/school.auth';
 import { BookController } from '../controllers/book.controller';
+import multer from 'multer';
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 const BookRouter = Router();
 
-BookRouter.post('/', auth, BookController.create);
+BookRouter.post('/', auth, upload.single('thumbnail'),  BookController.create);
 BookRouter.get('/', auth, BookController.get);
 BookRouter.get('/:id', auth, BookController.getById);
 
